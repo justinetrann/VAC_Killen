ea4b2cd004ed57d3bb3db43158d3ade4b5ccc2da
SushiAbuse <justine.tran@mavs.uta.edu>
2024-02-23 17:08:08 -0600

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import './Login.css';
 
 function Login() {
   return (
     <div className="Login">
       <Navbar />
+    
+    <div style={{ height: '1000px' }}/>
     </div>
   );
 }
