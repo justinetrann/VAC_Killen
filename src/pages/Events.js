24ada9f28fc25387fa305e0eef99d689cc357db7
SushiAbuse <justine.tran@mavs.uta.edu>
2024-02-25 21:14:21 -0600

    css

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -3,12 +3,12 @@ import Navbar from '../components/Navbar';
 import './Events.css';
 
 function Events() {
-   return (
-     <div>
-       <Navbar />
-       <h1>events</h1>
-     </div>
-   );
- }
- 
- export default Events;
+  return (
+    <div className="Events"> 
+      <Navbar />
+      <h1>events</h1>
+    </div>
+  );
+}
+
+export default Events;
