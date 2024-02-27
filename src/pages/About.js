bc348bf9d1bc297a04c69ce2ae91b0c5b7fd14c2
SushiAbuse <justine.tran@mavs.uta.edu>
2024-02-27 16:58:57 -0600

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -120,7 +120,6 @@ function About() {
                 ['bold', 'italic', 'underline', 'strike', 'blockquote'],
                 [{'list': 'ordered'}, {'list': 'bullet'},
                  {'indent': '-1'}, {'indent': '+1'}],
-                ['link', 'image', 'video'],
                 ['clean']
               ],
               imageResize: {},
